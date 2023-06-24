4f17cc81c16e17ca7c455e79b62029c5de95f117
KsyushaMalinina <121488994+KsyushaMalinina@users.noreply.github.com>
2023-06-24 23:56:23 +0700

    Update productRouter.js

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -6,11 +6,11 @@ const checkRole = require('../middleware/checkRoleMiddleware')
 router.post('/', checkRole('ADMIN'), productController.create)
 router.get('/', productController.getAll)
 router.get('/:id', productController.getOne)
-router.post('/update/:id',checkRole('ADMIN'), productController.updated)
-router.post('/del/:id',checkRole('ADMIN'), productController.delOne)
+router.put('/update/:id',checkRole('ADMIN'), productController.updated)
+router.delete('/del/:id',checkRole('ADMIN'), productController.delOne)
 router.get('/:id/similar', productController.getSimilarProducts);
 router.get('/inventory', productController.getAllForInventory)
-router.post('/status', productController.updateProductStatus)
-router.post('/:productId/size/:sizeId', productController.updateProductSize)
+router.put('/status', productController.updateProductStatus)
+router.put('/:productId/size/:sizeId', productController.updateProductSize)
 
 module.exports = router
